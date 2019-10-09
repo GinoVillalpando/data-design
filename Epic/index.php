9d4fbc6ad7bf4dbf92ffbd71d66bfe286f250134
Gino Villalpando <grieveous@live.com>
2019-10-09 03:03:59 -0600

    modified nav menu to buttons corrected <head> and <html> tags

--- Epic/index.php
+++ Epic/index.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
- <html lang="en">
-  <head>
-	  <meta charset="UTF-8">
-	  <Title>Index of Personas, Use Cases & Interation Flow</Title>
-	  <link rel="stylesheet" type="text/css" href="index-styling.css">
-	  <link rel="stylesheet" type="text/css" href="persona.css">
-  </head>
- <body>
-	<div>
-			<button class="buttons"><a href="identifying-persona.php" title="Soooo Amazing" target="myFrame"> Our Amazing User </a></button>
-			<button class="buttons"><a href="user-story.php" title="They Want All The Things" target="myFrame"> What Our User Wants from NeoGAF </a></button>
-			<button class="buttons"><a href="case-interaction-flow.php" title="It's nice to want things" target="myFrame"> What Does Our User Typically Do On NeoGAF? </a></button>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<Title>Index of Personas, Use Cases & Interation Flow</Title>
+		<link rel="stylesheet" type="text/css" href="index-styling.css">
+		<link rel="stylesheet" type="text/css" href="persona.css">
+	</head>
+		<body>
+			<div>
+				<button class="buttons"><a href="identifying-persona.php" title="Soooo Amazing" target="myFrame"> Our Amazing User </a></button>
+				<button class="buttons"><a href="user-story.php" title="They Want All The Things" target="myFrame"> What Our User Wants from NeoGAF </a></button>
+				<button class="buttons"><a href="case-interaction-flow.php" title="It's nice to want things" target="myFrame"> What Does Our User Typically Do On NeoGAF? </a></button>
 				<iframe name="myFrame"
-					  src="https://mydata2017.org/wp-content/uploads/sites/6/2017/01/mydata_design_icon_web.jpg" class="myFrame">
+						  src="https://mydata2017.org/wp-content/uploads/sites/6/2017/01/mydata_design_icon_web.jpg" class="myFrame">
 				</iframe>
-	</div>
- </body>
+			</div>
+		</body>
 </html>
