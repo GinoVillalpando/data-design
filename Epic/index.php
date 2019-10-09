7127b9569b2c5c8c9ec226a7681df4ad977ec6d2
Gino Villalpando <grieveous@live.com>
2019-10-09 03:07:13 -0600

    modified nav menu to buttons corrected <head> and <html> tags and removed unused <div>

--- Epic/index.php
+++ Epic/index.php
@@ -7,13 +7,11 @@
 		<link rel="stylesheet" type="text/css" href="persona.css">
 	</head>
 		<body>
-			<div>
-				<button class="buttons"><a href="identifying-persona.php" title="Soooo Amazing" target="myFrame"> Our Amazing User </a></button>
-				<button class="buttons"><a href="user-story.php" title="They Want All The Things" target="myFrame"> What Our User Wants from NeoGAF </a></button>
-				<button class="buttons"><a href="case-interaction-flow.php" title="It's nice to want things" target="myFrame"> What Does Our User Typically Do On NeoGAF? </a></button>
-				<iframe name="myFrame"
-						  src="https://mydata2017.org/wp-content/uploads/sites/6/2017/01/mydata_design_icon_web.jpg" class="myFrame">
+		<button class="buttons"><a href="identifying-persona.php" title="Soooo Amazing" target="myFrame"> Our Amazing User </a></button>
+		<button class="buttons"><a href="user-story.php" title="They Want All The Things" target="myFrame"> What Our User Wants from NeoGAF </a></button>
+		<button class="buttons"><a href="case-interaction-flow.php" title="It's nice to want things" target="myFrame"> What Does Our User Typically Do On NeoGAF? </a></button>
+		<iframe name="myFrame"
+				  src="https://mydata2017.org/wp-content/uploads/sites/6/2017/01/mydata_design_icon_web.jpg" class="myFrame">
 				</iframe>
-			</div>
 		</body>
 </html>
