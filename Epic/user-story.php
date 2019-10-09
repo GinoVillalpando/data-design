45083c497d1a341071b20a8d2ee0c1ef96fcba5a
Gino Villalpando <grieveous@live.com>
2019-10-09 14:45:27 -0600

    modified nav menu to buttons corrected <head> and <html> tags and removed unused <div> changed identity details on the persona changed user story for a power user without moderation tools changed interaction history based on a power user changed name for user story quote

--- Epic/user-story.php
+++ Epic/user-story.php
@@ -10,7 +10,7 @@
 			<p><q cite="https://twitter.com/nichegamer/status/1181581677207752704">As a power user I want more social
 					features for the threads and conversations I create to keep an exciting social environment.</q>
 				-
-			<a href="https://twitter.com/nichegamer/status/1181581677207752704"><em> Niche Gamer</em></a></p><hr>
+			<a href="https://twitter.com/nichegamer/status/1181581677207752704"><em> Roger Weeb </em></a></p><hr>
 		</div>
 	</body>
 </html>
