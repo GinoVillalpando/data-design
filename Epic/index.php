ed2b1b01641cea976a7320cc936522aee7ac0097
Gino Villalpando <grieveous@live.com>
2019-10-09 23:07:21 -0600

    modified nav menu to buttons corrected <head> and <html> tags and removed unused <div> changed identity details on the persona changed user story for a power user without moderation tools changed interaction history based on a power user changed name for user story quote added OS for computer that they own and what type of PC it is added Conceptual model and Entity Relationship Diagram png

--- Epic/index.php
+++ Epic/index.php
@@ -7,11 +7,18 @@
 		<link rel="stylesheet" type="text/css" href="persona.css">
 	</head>
 		<body>
-		<button class="buttons"><a href="identifying-persona.php" title="Soooo Amazing" target="myFrame"> Our Amazing User </a></button>
-		<button class="buttons"><a href="user-story.php" title="They Want All The Things" target="myFrame"> What Our User Wants from NeoGAF </a></button>
-		<button class="buttons"><a href="case-interaction-flow.php" title="It's nice to want things" target="myFrame"> What Does Our User Typically Do On NeoGAF? </a></button>
-		<iframe name="myFrame"
-				  src="https://mydata2017.org/wp-content/uploads/sites/6/2017/01/mydata_design_icon_web.jpg" class="myFrame">
+		<button class="buttons"><a href="identifying-persona.php" title="Soooo Amazing" target="myFrame"> Our Amazing User
+			</a></button>
+		<button class="buttons"><a href="user-story.php" title="They Want All The Things" target="myFrame"> What Our User
+				Wants from NeoGAF </a></button>
+		<button class="buttons"><a href="case-interaction-flow.php" title="What is he even doing?" target="myFrame">
+				What Does Our User Typically Do On NeoGAF? </a></button>
+				<button class="buttons"><a href="Conceptual-Model.html" title="Conceptual Diagram" target="myFrame">
+				Conceptual Model and Entity Relationship Diagram
+				</a></button>
+		<button class="buttons"><a href="ERDDiagram.png" title="ERD" target="myFrame">Entity Diagram</a></button><br>
+		<iframe name="myFrame" src="https://mydata2017.org/wp-content/uploads/sites/6/2017/01/mydata_design_icon_web.jpg"
+				  class="myFrame">
 				</iframe>
 		</body>
 </html>
