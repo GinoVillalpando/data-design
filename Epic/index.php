759dbc71f6ee998db60dd2cd654ae454263925cd
Gino Villalpando <grieveous@live.com>
2019-10-09 23:58:46 -0600

    added Conceptual Model and Entity Relationship Diagram png

--- Epic/index.php
+++ Epic/index.php
@@ -7,19 +7,19 @@
 		<link rel="stylesheet" type="text/css" href="persona.css">
 	</head>
 		<body>
-		<div class="div">
-		<button class="buttons"><a href="identifying-persona.php" title="Soooo Amazing" target="myFrame"> Our Amazing User
-			</a></button>
-		<button class="buttons"><a href="user-story.php" title="They Want All The Things" target="myFrame"> What Our User
+			<div class="div">
+				<button class="buttons"><a href="identifying-persona.php" title="Soooo Amazing" target="myFrame"> Our Amazing User
+					</a></button>
+				<button class="buttons"><a href="user-story.php" title="They Want All The Things" target="myFrame"> What Our User
 				Wants from NeoGAF </a></button>
-		<button class="buttons"><a href="case-interaction-flow.php" title="What is he even doing?" target="myFrame">
+				<button class="buttons"><a href="case-interaction-flow.php" title="What is he even doing?" target="myFrame">
 				What Does Our User Typically Do On NeoGAF? </a></button>
 				<button class="buttons"><a href="Conceptual-Model.html" title="Conceptual Diagram" target="myFrame">
 				Conceptual Model
-				</a></button>
-		<button class="buttons"><a href="ERDDiagram.png" title="ERD" target="myFrame"> Entity Relationship Diagram</a></button><br>
-		<iframe name="myFrame" src="https://images.unsplash.com/photo-1511376777868-611b54f68947?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-				  class="myFrame"></iframe>
-		</div>
+					</a></button>
+				<button class="buttons"><a href="ERDDiagram.png" title="ERD" target="myFrame"> Entity Relationship Diagram</a></button><br>
+					<iframe name="myFrame" src="https://images.unsplash.com/photo-1511376777868-611b54f68947?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+							  class="myFrame"></iframe>
+			</div>
 		</body>
 </html>
