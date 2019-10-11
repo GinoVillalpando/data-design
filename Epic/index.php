d523b6882004bf7d22452d521a9109d39a471a99
Gino Villalpando <grieveous@live.com>
2019-10-10 21:15:58 -0600

    removed unnecessary keys and changed ERD relationship

--- Epic/index.php
+++ Epic/index.php
@@ -8,16 +8,16 @@
 	</head>
 		<body class="bodycolor">
 			<div>
-				<a href="identifying-persona.php" title="Soooo Amazing" target="myFrame"><button class="buttons"> Our Amazing User
-					</button></a>
-				<a href="user-story.php" title="They Want All The Things" target="myFrame"><button class="buttons"> What Our User
-				Wants from NeoGAF </button></a>
-				<a href="case-interaction-flow.php" title="What is he even doing?" target="myFrame"><button class="buttons">
-				What Does Our User Typically Do On NeoGAF? </button></a>
-				<a href="Conceptual-Model.html" title="Conceptual Diagram" target="myFrame"><button class="buttons">
+				<a href="identifying-persona.php" title="Soooo Amazing" target="myFrame" class="buttons"> Our Amazing User
+					</a>
+				<a href="user-story.php" title="They Want All The Things" target="myFrame" class="buttons"> What Our User
+				Wants from NeoGAF</a>
+				<a href="case-interaction-flow.php" title="What is he even doing?" target="myFrame" class="buttons">
+				What Does Our User Typically Do On NeoGAF?</a>
+				<a href="Conceptual-Model.html" title="Conceptual Diagram" target="myFrame" class="buttons">
 				Conceptual Model
-					</button></a>
-				<a href="ERDDiagram.png" title="ERD" target="myFrame"><button class="buttons"> Entity Relationship Diagram</button></a><br>
+					</a>
+				<a href="ERDDiagram.png" title="ERD" target="myFrame" class="buttons">Entity Relationship Diagram</a><br>
 					<iframe name="myFrame" src="photo-1511376777868-611b54f68947.jpeg"
 							  class="myFrame"></iframe>
 			</div>
